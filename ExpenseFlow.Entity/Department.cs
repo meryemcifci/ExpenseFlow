f458ad39cb7e844ef17250441ba892039155404f
Meryem ÇİFCİ <meryem.cifci@ogr.sakarya.edu.tr>
2025-11-24 21:30:50 +0300

    Domain entityleri (User, Expense, Category, Department) oluşturuldu.  #2

---
+++ ExpenseFlow.Entity/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseFlow.Entity
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } //"Yazılım", "Muhasebe"
+
+      
+        // Bir departmanda birden çok çalışan olur.
+        public List<AppUser> Users { get; set; }
+    }
+}
