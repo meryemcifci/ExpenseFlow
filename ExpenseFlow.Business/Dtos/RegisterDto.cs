23ce56b819369eef323267f0ee8470225d9edd7b
Meryem ÇİFCİ <meryem.cifci@ogr.sakarya.edu.tr>
2025-11-27 22:12:56 +0300

    feat: identity ile auth service eklendi (login, register, rol ve departman atama)

---
+++ ExpenseFlow.Business/Dtos/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseFlow.WebUI.Models.Dtos
+{
+    public class RegisterDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
